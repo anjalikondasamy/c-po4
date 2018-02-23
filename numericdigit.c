857abc086e14d3857c1c7666fd5b13e49c830376
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-02-23 16:14:08 +0530

    c pgm
    good

---
+++ numericdigit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+void main()
+{
+    int a=123,n;
+    clrscr();
+    
+    while(a<0);
+    {
+        n=a%10;
+        printf("%d",n);
+    }
+   
+}
